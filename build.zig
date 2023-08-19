84edee83aee8a3df0bc8c54d0d20c36a10120744
Axojhf <20625305+axojhf@users.noreply.github.com>
2023-08-19 10:38:28 +0800

    fix

--- build.zig
+++ build.zig
@@ -1,6 +1,10 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+    _ = target;
+    _ = optimize;
     _ = b.addModule("zigwin32", .{
         .source_file = .{ .path = "win32.zig" },
     });
